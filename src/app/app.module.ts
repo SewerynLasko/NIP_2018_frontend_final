e3230c3c55cb965211c67781ed7754c6a219fe7d
Seweryn Lasko <Seweryn.Lasko@krolldiscovery.com>
2018-11-18 17:05:09 +0100

    Added NG Prime, added p-table component to the blog list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,13 @@ import { AppComponent } from './app.component';
 import { BlogPostListComponent } from './components/blog-post-list/blog-post-list.component';
 import { BlogPostComponent } from './components/blog-post/blog-post.component';
 import { HttpService } from './services/http.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      BlogPostListComponent,
-      BlogPostComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule
-   ],
-   providers: [
-      HttpService
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, BlogPostListComponent, BlogPostComponent],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, TableModule],
+  providers: [HttpService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
