c227a47a5f02f3a1ca81930d77f71caaedfbc1d2
Filip Łada <filip.lada@krollontrack.com>
2018-11-19 14:15:24 +0100

    Quick fix for DELETE request

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,22 +1,24 @@
-import { Settings } from '../utils/settings';
-import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+
+import { environment } from '../../environments/environment';
 import { BlogPost } from '../models/blogPost';
+import { Settings } from '../utils/settings';
 
 @Injectable()
 export class HttpService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
   public getPosts(): Observable<BlogPost[]> {
     return this.httpClient.get<BlogPost[]>(environment.ENDPOINT_URL + Settings.BLOG_POSTS_V2_CONTROLLER_URL);
   }
 
   public deletePost(postId: number): Observable<any> {
-    let httpParams = new HttpParams();
-    httpParams = httpParams.append('id', postId.toString());
+    //let httpParams = new HttpParams();
+    //httpParams = httpParams.append('id', postId.toString());
 
-    return this.httpClient.delete(environment.ENDPOINT_URL + Settings.BLOG_POSTS_V2_CONTROLLER_URL, { params: httpParams });
+    // return this.httpClient.delete(environment.ENDPOINT_URL + Settings.BLOG_POSTS_V2_CONTROLLER_URL, { params: httpParams });
+    return this.httpClient.delete(environment.ENDPOINT_URL + Settings.BLOG_POSTS_V2_CONTROLLER_URL + '/' + postId);
   }
 }
